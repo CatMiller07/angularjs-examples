bc1ac8d154c57c0e67533d26f7c1c287704fecf0
Cathy A. Miller <cat.miller07.cm.gmail.com>
2016-06-21 20:32:18 -0400

    second commit

--- order-form/script.js
+++ order-form/script.js
@@ -3,45 +3,60 @@ function OrderFormController($scope){
 	// Define the model properties. The view will loop
 	// through the services array and genreate a li
 	// element for every one of its items.
+  $scope.Qty = 0;
 
 	$scope.services = [
 		{
 			name: 'Web Development',
 			price: 300,
+			qty: 1,
 			active:true
 		},{
-			name: 'Design',
+			name: 'Design        ',
 			price: 400,
+			qty:2,
 			active:false
 		},{
 			name: 'Integration',
 			price: 250,
+			qty: 1,
 			active:false
 		},{
-			name: 'Training',
+			name: 'Training      ',
 			price: 220,
+			qty:3,
 			active:false
 		}
 	];
+
 
 	$scope.toggleActive = function(s){
 		s.active = !s.active;
 	};
 
-	// Helper method for calculating the total price
+	// Helper method for calculating the line price
+	$scope.lineTotal = function(s){
+			var lineTotal=0;
+			if (s.active){
+						lineTotal+= (s.price * s.qty);
+			}
+		  return lineTotal;
+	};
 
-	$scope.total = function(){
+	// Helper method for calculating the total price
+	$scope.getTotal = function(){
 
 		var total = 0;
-
+    var lineTotal=0;
 		// Use the angular forEach helper method to
 		// loop through the services array:
 
 		angular.forEach($scope.services, function(s){
 			if (s.active){
-				total+= s.price;
+			    	total += (s.price * s.qty);
 			}
 		});
+
 
 		return total;
 	};
